e4828f13e173a9b14e2280036fb71a333b98af5d
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-01 12:35:01 +0100

    modify comments

--- src/main/java/com/piotrs/medium/RunLength.java
+++ src/main/java/com/piotrs/medium/RunLength.java
@@ -1,14 +1,16 @@
 package com.piotrs.medium;
 
-//Difficulty: Medium
-//Have the function RunLength(str) take the str parameter being passed and return a compressed version of the string
-//        using the Run-length encoding algorithm. This algorithm works by taking the occurrence of each repeating
-//        character and outputting that number along with a single character of the repeating sequence.
-//        For example: "wwwggopp" would return 3w2g1o2p. The string will not contain any numbers, punctuation, or symbols.
+/*
+Difficulty: Medium
+Have the function RunLength(str) take the str parameter being passed and return a compressed version of the string
+using the Run-length encoding algorithm. This algorithm works by taking the occurrence of each repeating
+character and outputting that number along with a single character of the repeating sequence.
+For example: "wwwggopp" would return 3w2g1o2p. The string will not contain any numbers, punctuation, or symbols.
+*/
 
 public class RunLength {
 
-    private static final String STR = "wwwppoqq";
+    private static final String STR = "wwwggopp";
 
     public static void main(String[] args) {
 
