07029ac2ee1179c2507163291a2754dc02342b48
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-01 11:40:03 +0100

    fix algorithm

--- src/main/java/com/piotrs/SimpleCode.java
+++ src/main/java/com/piotrs/SimpleCode.java
@@ -7,25 +7,18 @@ public class SimpleCode {
     public static void main(String[] args) {
 
         int i = 0;
-        char controlChar = STR.charAt(0);
         StringBuilder sb = new StringBuilder();
-        while (i < STR.length() - 1) {
+        sb.append(STR).append(' ');
+        StringBuilder result = new StringBuilder();
+        while (sb.charAt(i) != ' ') {
             int counter = 0;
-            controlChar = STR.charAt(i);
-            while (STR.charAt(i) == controlChar) {
+            char controlChar = sb.charAt(i);
+            while (sb.charAt(i) == controlChar) {
                 counter += 1;
-                if (i < STR.length() - 1) {
-                    i += 1;
-                } else {
-                    break;
-                }
+                i += 1;
             }
-            sb.append(counter).append(controlChar);
+            result.append(counter).append(controlChar);
         }
-        if (STR.length() > 1) {
-            System.out.println(sb.toString());
-        } else {
-            System.out.println("1" + controlChar);
-        }
+        System.out.println(result.toString());
     }
 }
